f42f7368e340414c4ea9fd8d2ef3719747058a38
Pritam Khan Boni <pritam18@users.noreply.github.com>
2021-01-30 11:57:16 +0600

    Goal knowledge.

---
+++ Python_Files/Goal_Knowledge/__init__.py
@@ -0,0 +1,22 @@
+def get_goal_process():
+    print("Hello")
+
+
+def get_goal_data_type():
+    print("Hello")
+
+
+def get_goal_model_type():
+    print("Hello")
+
+
+def get_goal_target():
+    print("Hello")
+
+
+def get_goal_location():
+    print("Hello")
+
+
+def extract_goal_knowledge(input_file):
+    print('hello')
